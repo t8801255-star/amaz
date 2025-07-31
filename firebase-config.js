c41e88fd3ab5f2adfe113d5f1dd54fc8068dff02
t8801255-star <t8801255@gmail.com>
2025-07-31 13:42:49 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBO6eURV_qAqSmFLOt6CuzbjD2Q5ZrxQas",
+  authDomain: "amaz-d2a2c.firebaseapp.com",
+  projectId: "amaz-d2a2c",
+  storageBucket: "amaz-d2a2c.firebasestorage.app",
+  messagingSenderId: "613750109216",
+  appId: "1:613750109216:web:f758e71a10676656897845",
+  measurementId: "G-T17JCPV2Y1"
+};
